2fbfa06d83c219cce4943260a76b252b47ac4a28
homeshwarnelakuthi <homeshwarnelakurthi55@gmail.com>
2024-11-30 23:44:08 -0500

    main

--- health_records/integrity.py
+++ health_records/integrity.py
@@ -4,4 +4,5 @@ def generate_hash(data):
     hash_object = hashlib.sha256(data.encode('utf-8'))
     return hash_object.hexdigest()
 
-
+def verify_data_integrity(data, hash_value):
+    return generate_hash(data) == hash_value
