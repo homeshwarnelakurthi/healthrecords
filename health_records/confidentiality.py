1692bd43d626658db23c0d637b940931eb7b514d
homeshwarnelakuthi <homeshwarnelakurthi55@gmail.com>
2024-11-30 23:35:16 -0500

    updatedcd

--- health_records/confidentiality.py
+++ health_records/confidentiality.py
@@ -1,11 +1,7 @@
 from Crypto.Cipher import AES
 import base64
 
-def pad_data(data):
-    block_size = AES.block_size
-    padding_length = block_size - len(data) % block_size
-    padding = chr(padding_length) * padding_length
-    return data + padding
+
 
 def unpad_data(data):
     padding_length = ord(data[-1])
